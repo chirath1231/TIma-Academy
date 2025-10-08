1f178669e666445d4cfc0268166f865eb043a02a
chirath <146576470+chirath1231@users.noreply.github.com>
2025-10-08 23:59:11 +0530

    mmm

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+// backend/models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fullname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Student", userSchema, "registerDetails");
